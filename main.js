1919a5749f8b66631e0dfcd74402db40f10c326a
Mayank Phadke <mphadke1@hawk.iit.edu>
2020-11-23 20:56:57 -0600

    new design

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-console.log("SUCCESS")
-$(function() {
-        
-    $('.list-group-item').on('click', function() {
-      $('.fas', this)
-        .toggleClass('fa-chevron-right')
-        .toggleClass('fa-chevron-down');
+$(document).ready(function() {
+    console.log("TEST");
+    var navHeight = $('nav').outerHeight(); // get height of nav tag
+    $('ul a').click(function (e) {
+        e.preventDefault();
+        var target = $(this.hash);
+        $('html, body').animate({
+            scrollTop: target.offset().top - navHeight // scroll to h3 minus height of nav
+        }, 500);
+        return false;
     });
-  
+
+    $("#datetimepicker").datetimepicker()
 });
